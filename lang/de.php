f72f10c28f123227e74f8dd70e4b61ceef3c394e
Christophe TETTARASSAR <tetta.chris@gmail.com>
2013-12-16 18:27:34 +0100

    Create de.php

---
+++ lang/de.php
@@ -0,0 +1,25 @@
+<?php
+
+	// Language : german
+	
+	
+	$labelTitle = 'ISIChat - Der einfache Chat !';
+	$labelSurname = 'Ihren Spitznamen : ';
+	
+	$labelWelcome = 'Willkommen ';
+	$labelHello = '-_-_-_-_-_-_-_- Hallo! Ich bin Online. -_-_-_-_-_-_-_-';
+	
+	$labelExit = 'Ausfahrt Chat';
+	$labelConfirmExit = 'Sind Sie sicher, dass Sie die Sitzung beenden möchten?';
+	$labelBye = '-_-_-_-_-_-_-_- Ich bin abgemeldet. Auf Wiedersehen! -_-_-_-_-_-_-_-';
+	
+	$labelLogout = 'Sie haben sich erfolgreich abgemeldet.';
+	$labelError = 'Fehler : Bitte geben Sie einen gültigen und frei Namen ein';
+	$labelExpire = 'Ihre Sitzung ist abgelaufen';
+	
+	$labelMsg = 'Ihre Nachricht :';
+	
+	$labelFriends = 'Besucher online :'
+	
+	
+?>
