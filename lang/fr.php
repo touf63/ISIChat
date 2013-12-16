9ffc6d259a29945028b7a03d77332f4a7dc42232
Christophe TETTARASSAR <tetta.chris@gmail.com>
2013-12-16 18:26:21 +0100

    Create fr.php

---
+++ lang/fr.php
@@ -0,0 +1,25 @@
+<?php
+
+	// Language : french
+	// Default language
+
+	$labelTitle = 'ISIChat - Le chat facile !';
+	$labelSurname = 'Votre surnom : ';
+	
+	$labelWelcome = 'Bienvenue ';
+	$labelHello = '-_-_-_-_-_-_-_- Bonjour! je suis en ligne. -_-_-_-_-_-_-_-';
+	
+	$labelExit = 'Quitter le chat';
+	$labelConfirmExit = 'Etes-vous sur de vouloir mettre fin à votre session ?';
+	$labelBye = '-_-_-_-_-_-_-_- Je suis hors-ligne, au revoir ! -_-_-_-_-_-_-_-';
+	
+	$labelLogout = 'Vous vous êtes deconnecté avec succès.';
+	$labelError = 'Erreur : ce surnom n\'est pas valide ou est déjà pris : ';
+	$labelExpire = 'Votre session à expiré';
+	
+	$labelMsg = 'Votre message :';
+	
+	$labelFriends = 'Utilisateurs en ligne :'
+	
+
+?>
