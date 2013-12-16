6c49c1090eb75192ab51f1727cd4eb658546d2c2
Christophe TETTARASSAR <tetta.chris@gmail.com>
2013-12-16 18:28:16 +0100

    Create es.php

---
+++ lang/es.php
@@ -0,0 +1,25 @@
+<?php
+
+	// Language : spanish
+	
+	
+	$labelTitle = 'ISIChat - El chat fácil!';
+	$labelSurname = 'Su pseudónimo : ';
+	
+	$labelWelcome = 'Bienvenido ';
+	$labelHello = '-_-_-_-_-_-_-_- ¡Hola! Estoy en línea. -_-_-_-_-_-_-_-';
+	
+	$labelExit = 'Salida de chat';
+	$labelConfirmExit = '¿Está seguro que desea terminar la sesión?';
+	$labelBye = '-_-_-_-_-_-_-_- Cerré la sesión, ¡Adiós! -_-_-_-_-_-_-_-';
+	
+	$labelLogout = 'Se desconecta con éxito.';
+	$labelError = 'Error : Por favor ingrese un pseudónimo válido y disponible';
+	$labelExpire = 'Su sesión ha expirado';
+	
+	$labelMsg = 'Su mensaje :';
+	
+	$labelFriends = 'Usuarios en línea :'
+	
+	
+?>
