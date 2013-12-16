296a07bb225d23d4f5c3a10282b93d730333ecde
Christophe TETTARASSAR <tetta.chris@gmail.com>
2013-12-16 18:27:14 +0100

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,24 @@
+<?php
+
+	// Language : english
+	
+	
+	$labelTitle = 'ISIChat - The easy Chat !';
+	$labelSurname = 'Your nickname : ';
+	
+	$labelWelcome = 'Welcome ';
+	$labelHello = '-_-_-_-_-_-_-_- Hello! I am online. -_-_-_-_-_-_-_-';
+	
+	$labelExit = 'Exit Chat';
+	$labelConfirmExit = 'Are you sure you want to end the session?';
+	$labelBye = '-_-_-_-_-_-_-_- I am logged out, goodbye ! -_-_-_-_-_-_-_-';
+	
+	$labelLogout = 'You have successfully logged out.';
+	$labelError = 'Error : please enter a valid and available name';
+	$labelExpire = 'Your session has expired';
+	
+	$labelMsg = 'Your message :';
+	
+	$labelFriends = 'Online users :'
+	
+?>
